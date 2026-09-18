564bb8597d3c9d99fe33c43cf583f60783ab3fa1
William dos Santos Hennemann <williamdossantoshennemann@gmail.com>
2026-09-18 16:22:16 -0300

    arrumada

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ angular.module('blog', []);
 
 angular.module('blog').controller('Rest', function ($scope, $http) {
   $http.get('https://api-fake-blog-1.onrender.com/postagens')
-    .then(function(response) {
-        $scope.publicacoes = response.data; 
+    .then(function (response) {
+      $scope.publicacoes = response.data;
     })
-    .catch(function(error) {
-        console.error("Erro ao carregar postagens:", error);
+    .catch(function (error) {
+      console.error('Erro ao carregar postagens:', error);
     });
 });
