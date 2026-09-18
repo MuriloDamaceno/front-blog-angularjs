e7c158adcffe648aaf87653debc1b8b0f8af8191
William dos Santos Hennemann <williamdossantoshennemann@gmail.com>
2026-09-18 15:39:09 -0300

    Mudando para conectar com a api

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 angular.module('blog', []);
-angular.module('blog').controller('Rest', function ($scope, $http){
-  $http.get('https://api-fake-blog.onrender.com/postagens').
-      success(function(data) {
-          $scope.publicacoes = data;
-      }
-  );
+
+angular.module('blog').controller('Rest', function ($scope, $http) {
+  $http.get('https://api-fake-blog-1.onrender.com/postagens')
+    .then(function(response) {
+        $scope.publicacoes = response.data; 
+    })
+    .catch(function(error) {
+        console.error("Erro ao carregar postagens:", error);
+    });
 });
